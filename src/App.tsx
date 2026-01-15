98904c6f70e0dbaa283cad9e6819e4a501ef6722
NguyenChiThuan0509 <ncthuan0509@gmail.com>
2026-01-15 13:49:52 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <p>Hi, App Blogs</p>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<UserListPage />} />
+    //     <Route path="/create" element={<CreateUserPage />} />
+    //     <Route path="/edit/:id" element={<EditUserPage />} />
+    //   </Routes>
+    // </BrowserRouter>
+  );
+}
+
+export default App;
